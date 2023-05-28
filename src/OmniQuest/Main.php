36d00315a8c9034cca4bf07c4df2fe58b6a28c3d
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-05-28 14:14:39 +0530

    update Main.php

--- src/OmniQuest/Main.php
+++ src/OmniQuest/Main.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace OmniQuest;
+
+use OmniQuest\Manager\QuestCommand;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase
+{
+    public static Main $main;
+    
+    protected function onLoad(): void
+    {
+        self::$main = $this;
+    }
+    public function onEnable(): void
+    {
+        $this->getLogger()->info("OmniQuest Enabled!");
+        $this->getServer()->getCommandMap()->register("quest", new QuestCommand());
+    }
+}
